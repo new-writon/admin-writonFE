5aa5d72a3f0c1cf14a582a32bff04140ada8c099
seungzzok <123801984+seungzzok@users.noreply.github.com>
2024-08-01 19:59:43 +0900

    feat: molecule ContentSection
    - 메인 컨텐츠 항목들에서 제목과 내용부분으로 포함되어있는 부분
    - 항목들마다 내용부분의 위치가 달라서 titleWidth 속성 추가

---
+++ src/components/molecules/ContentSection.tsx
@@ -0,0 +1,24 @@
+import { theme } from "../../styles/theme";
+import { FlexBox } from "../atoms";
+import { B2 } from "../atoms/Text";
+
+interface ContentSection {
+  children: React.ReactNode;
+  title: string;
+  titleWidth: number;
+}
+
+const ContentSection = ({ children, title, titleWidth }: ContentSection) => {
+  return (
+    <FlexBox>
+      <div style={{ width: `${titleWidth}px` }}>
+        <B2 weight="sb" color={theme.color.gray[80]}>
+          {title}
+        </B2>
+      </div>
+      {children}
+    </FlexBox>
+  );
+};
+
+export default ContentSection;
