cf907f2d92becf556ebd8c709a6352c0e6772c8d
seungzzok <123801984+seungzzok@users.noreply.github.com>
2024-08-05 16:56:18 +0900

    refactor: SideBar 정적데이터 분리

---
+++ src/data/SideBarMenu.ts
@@ -0,0 +1,30 @@
+const challengeMenuList = [
+  {
+    name: "챌린지 참여 현황",
+    path: "/challenge/dashboard",
+  },
+  {
+    name: "챌린지 정보",
+    path: "/challenge/info",
+  },
+  {
+    name: "챌린지 질문 관리",
+    path: "/challenge/question",
+  },
+  {
+    name: "기능 커스텀",
+    path: "/challenge/custom",
+  },
+];
+const participationMenuList = [
+  {
+    name: "참여자 정보",
+    path: "/participation/info",
+  },
+  {
+    name: "참여자 초대",
+    path: "/participation/participate",
+  },
+];
+
+export { challengeMenuList, participationMenuList };
