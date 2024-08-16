0e23e4232bd237e66e5fef104b0ebd58efd4cf96
seungzzok <123801984+seungzzok@users.noreply.github.com>
2024-08-17 03:14:13 +0900

    feat: Post AuthReissue API
    - 우선순위 낮은 작업으로 연동만 진행

--- src/apis/authAPI.ts
+++ src/apis/authAPI.ts
@@ -41,4 +41,48 @@ export const postAuthLogoutAPI = async () => {
   }
 };
 
+// 토큰 재발급 API
+export const postAuthReissueAPI = async () => {
+  try {
+    const {
+      data: {
+        data: { accessToken, refreshToken },
+      },
+    } = await Axios.post("/auth/reissue");
 
+    localStorage.setItem("accessToken", accessToken);
+    localStorage.setItem("refreshToken", refreshToken);
+  } catch (error: any) {
+    if (error.response) {
+      console.error("Server Error:", error.response.data);
+    } else {
+      console.error("Error creating question:", error.message);
+    }
+    throw error;
+  }
+};
+
+// const { data: relatedData } = useQuery({
+//     queryKey: ['related-products', productId],
+//     queryFn: () =>
+//       getCategoryProductsAPI({
+//         family: infoData?.family || '',
+//         sorter: 'REVIEW_COUNT_DESC',
+//         limit: 12,
+//       }),
+//     staleTime: 60 * 1000,
+//     enabled: isSuccess,
+//   });
+
+// const { mutate } = useMutation({
+//     mutationFn: () => postReviewRecommendAPI(data?.id),
+//     onSuccess: () => {
+//       setShowRecommended((prev) => !prev);
+//       showRecommended
+//         ? setShowRecommendCount((prev) => prev - 1)
+//         : setShowRecommendCount((prev) => prev + 1);
+//     },
+//     onError: (err) => {
+//       console.error(err);
+//     },
+//   });
