ea92d0a6496772ac77ca7599784f45bb239e901c
seungzzok <123801984+seungzzok@users.noreply.github.com>
2024-08-17 03:13:40 +0900

    feat: Post AuthLogout API
    - 로그아웃 버튼 부재로 연동만 진행
    - 버튼 적용 X

--- src/apis/authAPI.ts
+++ src/apis/authAPI.ts
@@ -23,3 +23,22 @@ export const postAuthLoginAPI = async (
     throw error;
   }
 };
+
+// 로그아웃 API
+export const postAuthLogoutAPI = async () => {
+  try {
+    await Axios.delete("/auth/logout");
+
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("refreshToken");
+  } catch (error: any) {
+    if (error.response) {
+      console.error("Server Error:", error.response.data);
+    } else {
+      console.error("Error creating question:", error.message);
+    }
+    throw error;
+  }
+};
+
+
