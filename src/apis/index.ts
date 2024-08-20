c20cf9212345a2e558079100f966b0e369a7b8b8
seungzzok <123801984+seungzzok@users.noreply.github.com>
2024-08-20 13:44:40 +0900

    feat: Get ParticipationEmail API

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -14,6 +14,7 @@ import {
 import { postOrganizationAPI } from "./organizationAPI";
 import {
   getParticipationInfoAPI,
+  getParticipationEmailAPI,
 } from "./participationAPI";
 
 export {
@@ -28,4 +29,5 @@ export {
   putChallengeQuestionsAPI,
   postOrganizationAPI,
   getParticipationInfoAPI,
+  getParticipationEmailAPI,
 };
