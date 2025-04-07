17880605fceba87df6780db7f46708b2fd7fba36
seungzzok <123801984+seungzzok@users.noreply.github.com>
2025-04-07 20:40:43 +0900

    feat: SideBar 반응형 구현
    - 화면이 작아질 경우 SideBar가 닫히고 화면이 커질 경우 열리고 고정
    - 토글 버튼을 누를 경우 사이드바가 열고 닫히는 기능 추가
    - 사이드바에 있는 각 메뉴를 클릭할 경우에도 사이드바 닫힘
    - fixed될 시 화면의 높이를 맞추기 위해 60px를 뺀 값으로 조정

--- src/components/molecules/SideBar.tsx
+++ src/components/molecules/SideBar.tsx
@@ -16,9 +16,10 @@ interface Menu {
   isTitle?: boolean;
   path?: string;
   children: React.ReactNode;
+  toggleSideBar?: () => void;
 }
 
-const Menu = ({ isTitle, children, path }: Menu) => {
+const Menu = ({ isTitle, children, path, toggleSideBar = () => {} }: Menu) => {
   const navigate = useNavigate();
   const location = useLocation();
   const pathname = location.pathname;
@@ -27,10 +28,15 @@ const Menu = ({ isTitle, children, path }: Menu) => {
   const isCurPath = pathname == path;
   const disabled = challengeList.length === 0;
 
+  const onClickMenu = () => {
+    toggleSideBar();
+    !isTitle && navigate(`${path}`);
+  };
+
   return (
     <MenuContainer
       as={isTitle ? "div" : "button"}
-      onClick={() => !isTitle && navigate(`${path}`)}
+      onClick={onClickMenu}
       disabled={disabled}
       $disabled={disabled}
     >
@@ -57,73 +63,84 @@ const SideBar = () => {
   };
 
   return (
-    <Container>
-      {/* ========== Top Info ========== */}
-      <FlexBox
-        fullWidth
-        align="center"
-        justify="space-between"
-        gap={12}
-        padding="0px 10px"
-        style={{ width: "240px" }}
-      >
-        <ImgConatainer>
-          {organizationLogo ? (
-            <img src={organizationLogo} alt="organizaiton_logo" />
-          ) : (
-            <BiBuildings size={24} color={theme.color.gray[80]} />
-          )}
-        </ImgConatainer>
-        <B2 weight="sb" color={theme.color.gray[100]} style={{ flex: 1 }}>
-          {organizationName}
-        </B2>
-        <Button
-          size="sm"
-          type="none"
-          onClick={() => {
-            navigate("/organization/edit");
-          }}
-        >
-          설정
-        </Button>
-      </FlexBox>
-      <Hyphen />
-
-      {/* ========== Menu Bar ========== */}
-      <FlexBox col fullWidth>
-        <Menu isTitle>챌린지 관리</Menu>
-        {challengeMenuList.map(({ name, path }) => (
-          <Menu key={path} path={path}>
-            {name}
-          </Menu>
-        ))}
-      </FlexBox>
-      <FlexBox col fullWidth>
-        <Menu isTitle>참여자 관리</Menu>
-        {participationMenuList.map(({ name, path }) => (
-          <Menu key={path} path={path}>
-            {name}
-          </Menu>
-        ))}
-      </FlexBox>
-      <Hyphen />
-
-      {/* ========== Button ========== */}
-      <ButtonContainer>
-        <Button
+    <>
+      <Container $isOpen={isSidebarOpen}>
+        {/* ========== Top Info ========== */}
+        <FlexBox
           fullWidth
-          rightPlus
-          size="lg"
-          type="light"
-          onClick={() => {
-            navigate("/challenge/create");
-          }}
-          disabled={location.pathname === "/challenge/create"}
+          align="center"
+          justify="space-between"
+          gap={12}
+          padding="0px 10px"
+          style={{ width: "240px" }}
         >
-          챌린지 개설
-        </Button>
-      </ButtonContainer>
-    </Container>
+          <ImgConatainer>
+            {organizationLogo ? (
+              <img src={organizationLogo} alt="organizaiton_logo" />
+            ) : (
+              <BiBuildings size={24} color={theme.color.gray[80]} />
+            )}
+          </ImgConatainer>
+          <B2 weight="sb" color={theme.color.gray[100]} style={{ flex: 1 }}>
+            {organizationName}
+          </B2>
+          <Button
+            size="sm"
+            type="none"
+            onClick={() => {
+              isSidebarOpen && toggleSidebar();
+              navigate("/organization/edit");
+            }}
+          >
+            설정
+          </Button>
+        </FlexBox>
+        <Hyphen />
+
+        {/* ========== Menu Bar ========== */}
+        <FlexBox col fullWidth>
+          <Menu isTitle>챌린지 관리</Menu>
+          {challengeMenuList.map(({ name, path }) => (
+            <Menu
+              key={path}
+              path={path}
+              toggleSideBar={isSidebarOpen ? toggleSidebar : undefined}
+            >
+              {name}
+            </Menu>
+          ))}
+        </FlexBox>
+        <FlexBox col fullWidth>
+          <Menu isTitle>참여자 관리</Menu>
+          {participationMenuList.map(({ name, path }) => (
+            <Menu
+              key={path}
+              path={path}
+              toggleSideBar={isSidebarOpen ? toggleSidebar : undefined}
+            >
+              {name}
+            </Menu>
+          ))}
+        </FlexBox>
+        <Hyphen />
+
+        {/* ========== Button ========== */}
+        <ButtonContainer>
+          <Button
+            fullWidth
+            rightPlus
+            size="lg"
+            type="light"
+            onClick={() => {
+              isSidebarOpen && toggleSidebar();
+              navigate("/challenge/create");
+            }}
+            disabled={location.pathname === "/challenge/create"}
+          >
+            챌린지 개설
+          </Button>
+        </ButtonContainer>
+      </Container>
       <MenuToggleButton onClick={toggleSidebar} $isOpen={isSidebarOpen}>
         {isSidebarOpen ? (
           <IoIosArrowBack size={24} color={theme.color.gray[80]} />
@@ -131,12 +148,13 @@ const SideBar = () => {
           <IoIosArrowForward size={24} color={theme.color.gray[80]} />
         )}
       </MenuToggleButton>
+    </>
   );
 };
 
 export default SideBar;
 
-const Container = styled.section`
+const Container = styled.section<{ $isOpen: boolean }>`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -145,6 +163,20 @@ const Container = styled.section`
   height: 100%;
   padding: 16px 10px;
   background-color: ${({ theme }) => theme.color.gray[10]};
+  box-shadow: 0px 14px 20px 0px rgba(33, 33, 33, 0.1);
+  z-index: 2;
+
+  transform: translateX(0);
+  transition: transform 0.3s ease-in-out;
+
+  @media ${({ theme }) => theme.device.tablet} {
+    transform: ${({ $isOpen }) =>
+      $isOpen ? "translateX(0)" : "translateX(-100%)"};
+    position: fixed;
+    height: calc(100% - 60px);
+  }
+`;
+
 const MenuToggleButton = styled.button<{ $isOpen: boolean }>`
   position: fixed;
   display: none;
